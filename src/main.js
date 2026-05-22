68e9cfd1196625be8270e8b24aecf6eadcf56949
zzonle <bastianofedamaldonado@gmail.com>
2026-05-22 19:02:15 -0400

    Uso de cloudinary, servicio de almacenamiento en la nube

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { Cloudinary } from '@cloudinary/url-gen'
 
 const app = createApp(App)
+
+export const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'dlqb8ofzp',
+  },
+})
 
 app.use(router)
 
